63c24fc8d8ed6cf91620435c7a34d1ee27831522
= <anotidaekutsvaira@gmail.com>
2026-04-06 23:37:27 +0200

    Adding a ProductS

--- src/main/java/servlet/ProductCatalogueServlet.java
+++ src/main/java/servlet/ProductCatalogueServlet.java
@@ -13,6 +13,9 @@ import service.ProductService;
 @WebServlet({"/" , "/products"})
 public class ProductCatalogueServlet extends HttpServlet {
 	
+
+	private static final long serialVersionUID = 1L;
+	
 	private final ProductService productService = new ProductService();
 	
 	@Override
